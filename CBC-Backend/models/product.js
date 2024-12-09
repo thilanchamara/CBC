abc1d9de741ac15c5f88121a9fd5456e3cc18e1b
thilanchamara <thilanchamara1997@gmail.com>
2024-12-09 16:02:59 +0530

    update the prdouct model

--- CBC-Backend/models/product.js
+++ CBC-Backend/models/product.js
@@ -1,9 +1,42 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String,
+  productId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  altName: [
+    {
+      type: String,
+    },
+  ],
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  price: {
+    type: Number,
+    required: true,
+  },
+  lastPrice: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  stockCount: {
+    type: Number,
+    required: true,
+  },
 });
 
 const Product = mongoose.model("Product", productSchema);
