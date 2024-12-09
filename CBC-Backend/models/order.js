7ee6b4221b49d70315d9f7c5d7f21c43c0c412fe
thilanchamara <thilanchamara1997@gmail.com>
2024-12-09 22:14:40 +0530

    create order model

---
+++ CBC-Backend/models/order.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  orderNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: Number,
+        required: true,
+      },
+      productId: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  paymentId: {
+    type: String,
+  },
+  note: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
